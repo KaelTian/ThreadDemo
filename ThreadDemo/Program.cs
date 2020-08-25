083e7d4a4f83c94646c1cb3db81db6c0b963c4cd
stian <sai.tian@avepoint.com>
2020-08-25 14:21:04 +0800

    Add project files.

---
+++ ThreadDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadDemo
+{
+    class Program
+    {
+        static readonly int loopCount = 1000;
+        static void Main(string[] args)
+        {
+            //Thread.CurrentThread.Name = "Main Thread ... ";
+            Thread t = new Thread(ThreadY);
+            t.Name = "Y thread";
+            t.Start();
+            t.Join();
+            // Console.WriteLine(Thread.CurrentThread.Name);
+            //for (var i = 0; i < loopCount; i++)
+            //{
+            //    Console.Write("X");
+            //}
+            Console.WriteLine("Thread t has ended!");
+            Console.Read();
+        }
+
+
+
+        static void ThreadY()
+        {
+            //Console.WriteLine(Thread.CurrentThread.Name);
+            for (var i = 0; i < loopCount; i++)
+            {
+                Console.Write("Y");
+            }
+        }
+    }
+}
