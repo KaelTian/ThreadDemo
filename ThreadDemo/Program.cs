1552b0c12ad1fcb14240d4a1bd0f6fd8c40e3b13
stian <sai.tian@avepoint.com>
2020-08-25 16:40:39 +0800

    shared thread 1

--- ThreadDemo/Program.cs
+++ ThreadDemo/Program.cs
@@ -5,21 +5,48 @@ namespace ThreadDemo
 {
     class Program
     {
-        static Thread thread1, thread2;
+        //static Thread thread1, thread2;
+        //static void Main(string[] args)
+        //{
+        //    thread1 = new Thread(ThreadProc);
+        //    thread1.Name = "Thread1";
+        //    thread1.Start();
+
+        //    thread2 = new Thread(ThreadProc);
+        //    thread2.Name = "Thread2";
+        //    thread2.Start();
+
+        //    Console.Read();
+        //}
+
+        //static void ThreadProc()
+        //{
+        //    Console.WriteLine("\nCurrent thread: {0}", Thread.CurrentThread.Name);
+        //    if (Thread.CurrentThread.Name == "Thread1" &&
+        //        thread2.ThreadState != ThreadState.Unstarted)
+        //    {
+        //        if (thread2.Join(2000))
+        //            Console.WriteLine("Thread2 has termminated.");
+        //        else
+        //            Console.WriteLine("The timeout has elapsed and Thread1 will resume.");
+        //    }
+
+        //    Thread.Sleep(2000);
+        //    Console.WriteLine("\nCurrent thread: {0}", Thread.CurrentThread.Name);
+        //    Console.WriteLine("Thread1: {0}", thread1.ThreadState);
+        //    Console.WriteLine("Thread2: {0}\n", thread2.ThreadState);
+        //}
+
+
+
         static void Main(string[] args)
         {
-            thread1 = new Thread(ThreadProc);
-            thread1.Name = "Thread1";
-            thread1.Start();
+            ThreadTest tt = new ThreadTest();
 
-            thread2 = new Thread(ThreadProc);
-            thread2.Name = "Thread2";
-            thread1.Start();
-        }
+            new Thread(tt.Go).Start();
+            tt.Go();
 
-        static void ThreadProc()
-        {
-
+            Console.Read();
         }
 
     }
---
+++ ThreadDemo/ThreadTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadDemo
+{
+    public class ThreadTest
+    {
+        private bool _done;
+
+
+        public void Go()
+        {
+            if (!_done)
+            {
+                _done = true;
+                Console.WriteLine("Done!");
+            }
+        }
+    }
+}
