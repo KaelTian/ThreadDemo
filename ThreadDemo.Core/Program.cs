3d03317111801533330b70f64b492fb530aa8d98
Kael.Tian <Kael_Tian@163.com>
2024-02-19 14:09:06 +0800

    add some core codes for Linux test

---
+++ ThreadDemo.Core/Program.cs
@@ -0,0 +1,8 @@
+﻿using ThreadDemo;
+
+ThreadTest tt = new ThreadTest();
+
+new Thread(tt.Go).Start();
+tt.Go();
+
+Console.Read();
---
+++ ThreadDemo.Core/ThreadTest.cs
@@ -0,0 +1,18 @@
+﻿namespace ThreadDemo
+{
+    public class ThreadTest
+    {
+        private bool _done;
+
+
+        public void Go()
+        {
+            if (!_done)
+            {
+                _done = true;
+                Console.WriteLine("Done!");
+                Console.WriteLine($"Current time:{DateTime.Now},thread id:{Thread.CurrentThread.ManagedThreadId}");
+            }
+        }
+    }
+}
